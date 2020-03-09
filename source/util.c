97a28567a24df73131ddaa796defe4ae4a4a8d2d
cathery <dumdumcathery@gmail.com>
2020-03-09 04:50:18 +0300

    Allow changing the keycombo of the server pausing Closes #2

--- source/util.c
+++ source/util.c
@@ -6,32 +6,77 @@
 #include <unistd.h>
 
 #include <switch.h>
+#include "minIni.h"
 
 static bool inputThreadRunning = true;
+static bool paused = false;
+static Mutex pausedMutex = 0;
+static Thread pauseThread;
+static HidControllerKeys comboKeys[8] = {};
 
 void inputPoller()
 {
     do
     {
         hidScanInput();
-        u64 kDown = 0;
         u64 kHeld = 0;
-        for (u8 controller = 0; controller != 8; controller++)
-        {
-            kDown |= hidKeysDown(controller);
+        for (u8 controller = 0; controller != 10; controller++)
             kHeld |= hidKeysHeld(controller);
-        }
 
-        if ((kDown & KEY_PLUS || kDown & KEY_MINUS || kDown & KEY_X) && (kHeld & KEY_PLUS && kHeld & KEY_MINUS && kHeld & KEY_X))
-            setPaused(!isPaused());
+        u64 keyCombo = 0;
+        for (u8 i=0; i != sizearray(comboKeys); ++i)
+            keyCombo |= comboKeys[i];
+
+        static bool keyComboPressed = false;
+
+        if ((kHeld & keyCombo) == keyCombo)
+        {
+            if (!keyComboPressed)
+            {
+                keyComboPressed = true;
+                setPaused(!isPaused());
+            }
+        }
+        else
+        {
+            keyComboPressed = false;
+        }
         svcSleepThread(1e+8);
     } while (inputThreadRunning);
 }
 
+const char* buttons[] = {
+    "A",
+    "B",
+    "X",
+    "Y",
+    "LS",
+    "RS",
+    "L",
+    "R",
+    "ZL",
+    "ZR",
+    "PLUS",
+    "MINUS",
+    "DLEFT",
+    "DUP",
+    "DRIGHT",
+    "DDOWN",
+};
 
-static bool paused = false;
-static Mutex pausedMutex = 0;
-static Thread pauseThread;
+HidControllerKeys GetKey(const char *text)
+{
+    for (u8 i=0; i != sizearray(buttons); ++i)
+    {
+        if (strcmp(text, buttons[i]) == 0)
+        {
+            return BIT(i);
+        }
+    }
+    return 0;
+}
+
+
 
 Result pauseInit() {
     Result rc;
@@ -41,6 +86,18 @@ Result pauseInit() {
     if (should_pause_file != NULL) {
         paused = true;
         fclose(should_pause_file);
+    }
+
+    {
+        char buffer[128];
+        ini_gets("Pause", "keycombo:", "PLUS+MINUS+X", buffer, 128, CONFIGPATH);
+        char* token = strtok(buffer, "+");
+        int i = 0;
+        while (token != NULL && i != sizearray(comboKeys))
+        {
+            comboKeys[i++] = GetKey(token);
+            token = strtok(NULL, "+");
+        };
     }
 
     inputThreadRunning = true;
